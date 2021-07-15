3182de7f40c4645b3ce80b2ad113a51922b83638
m-orlewski <morlewski@protonmail.com>
2021-07-15 19:32:02 +0200

    cubes

---
+++ mpl_cubes.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 5001)
+cubes = [x**3 for x in x_values]
+
+fig, ax = plt.subplots()
+
+ax.scatter(x_values, cubes, c=cubes, cmap=plt.cm.magma, s=3)
+
+plt.show()
