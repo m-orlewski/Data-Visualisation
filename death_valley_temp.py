51ef5c8034a5c8343792dacb29dd14f61c7fee2e
m-orlewski <morlewski@protonmail.com>
2021-07-16 14:19:34 +0200

    data and temperatures

---
+++ death_valley_temp.py
@@ -0,0 +1,36 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = 'data/death_valley_2018_simple.csv'
+with open(filename, 'r') as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+
+	dates, highs, lows = [], [], []
+	for row in reader:
+		current_date = datetime.strptime(row[2], '%Y-%m-%d')
+		try:
+			high = int(row[5])
+			low = int(row[6])
+		except ValueError:
+			print(f'Missing data for {current_date}')
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', linewidth=1)
+ax.plot(dates, lows, c='blue', linewidth=1)
+
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+plt.title("Daily high temperatures, 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate() #draws date labels diagonally
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
